6d3df0f774d9bf6a6ac3dcee92d0597488f9ae24
satvikc <mystic.satvik@gmail.com>
2013-03-14 11:51:25 +0530

    Sha1 Structure Changed to rounds of 16. Don't inline the rounds as it resulted in decreased performance.

--- raaz-hash-sha/Raaz/Hash/Sha/Sha1/Ref/Sha1.hs
+++ raaz-hash-sha/Raaz/Hash/Sha/Sha1/Ref/Sha1.hs
@@ -20,9 +20,17 @@ import Raaz.Util.Ptr
 import Raaz.Hash.Sha.Sha1.Type(SHA1(..))
 import Raaz.Hash.Sha.Sha1.Ref.Sha1TH
 
--- | roundF function generated from TH
-$(oneRound)
-{-# INLINE roundF #-}
+-- | round functions generated from TH
+$(round0f)
+{-# INLINE round0 #-}
+$(round1f)
+{-# INLINE round1 #-}
+$(round2f)
+{-# INLINE round2 #-}
+$(round3f)
+{-# INLINE round3 #-}
+$(round4f)
+{-# INLINE round4 #-}
 
 -- | Compresses one block.
 sha1CompressSingle :: SHA1
@@ -45,3 +53,8 @@ sha1CompressSingle (SHA1 h0 h1 h2 h3 h4) cptr = roundF h0 h1 h2 h3 h4
          <*> loadFromIndex cptr 13
          <*> loadFromIndex cptr 14
          <*> loadFromIndex cptr 15
+    where
+      roundF a b c d e m0 m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12 m13 m14 m15 =
+          let (a',b',c',d',e') = round4 $ round3 $ round2 $ round1 $ round0
+                (a,b,c,d,e,m0,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15)
+          in SHA1 (a+a') (b+b') (c+c') (d+d') (e+e')
--- raaz-hash-sha/Raaz/Hash/Sha/Sha1/Ref/Sha1TH.hs
+++ raaz-hash-sha/Raaz/Hash/Sha/Sha1/Ref/Sha1TH.hs
@@ -8,7 +8,7 @@ spliced in the actual module.
 {-# LANGUAGE TemplateHaskell #-}
 
 module Raaz.Hash.Sha.Sha1.Ref.Sha1TH
-       ( oneRound
+       ( round0f, round1f, round2f, round3f, round4f
        ) where
 
 import Control.Applicative
@@ -18,78 +18,139 @@ import Language.Haskell.TH
 import Raaz.Types
 import Raaz.Util.TH
 
-import Raaz.Hash.Sha.Sha1.Type(SHA1(..))
-
--- | Declares roundF function which denotes compression of one
--- block. This is used internally for efficient code generation using
--- Template Haskell.
-oneRound :: DecsQ
-oneRound = sequence $ [typeSig, funD name [cls]]
+-- | 0 - 15
+round0f :: DecsQ
+round0f = sequence $ [typeSig, funD name [cls]]
   where
-    name = mkName "roundF"
-    cls = clause (args1 ++ args2) (normalB (LetE <$> roundLoop <*>
-                                      [| SHA1
-                                         ($(a $ -1) + $(a 79))
-                                         ($(b $ -1) + $(b 79))
-                                         ($(c $ -1) + $(c 79))
-                                         ($(d $ -1) + $(d 79))
-                                         ($(e $ -1) + $(e 79))
-                                      |])) []
+    name = mkName "round0"
+    cls = clause [tupP $ args1 ++ args2] (normalB (LetE <$> step <*>
+                                                   tupE (out1 ++ out2))) []
     args1 = map (flip subP (-1 :: Int)) ["a","b","c","d","e"]
-    args2 = map (subP "m") [0..15 :: Int]
-    typeSig = sigD name $
-              foldl (const . appT wordtype) (conT ''SHA1) [1..21 :: Int]
-    wordtype = appT arrowT (conT ''Word32BE)
+    args2 = map (subP "w") [0..15 :: Int]
+    out1 = map (flip subE (15 :: Int)) ["a","b","c","d","e"]
+    out2 = map (subE "w") [16..31 :: Int]
+    step = declarations [wdecs,adecs,cdecs,restdecs] [0..15]
+    typeSig = sigD name $ appT (appT arrowT tup) tup
+    tup = foldl (\x _ -> appT x wordtype) (tupleT 21) [1..21 :: Int]
+    wordtype = (conT ''Word32BE)
 
--- | Unrolls the round loop.  Also assumes a__1,b__1,c__1,d__1,e__1,
--- which are the hash values in the previous round also present in
--- scope.
-roundLoop :: DecsQ
-roundLoop = declarations [wdecs,kdecs,adecs,cdecs,restdecs] [0..79]
+-- | 16 - 31
+round1f :: DecsQ
+round1f = sequence $ [typeSig, funD name [cls]]
   where
-    adecs :: Int -> DecsQ
-    adecs = variable' "a" ''Word32BE body
-      where
-        body j = [| $(r') + $(f') + $(e $ j-1) + $(k j) + $(w j) :: Word32BE |]
-         where r' = [| rotateL $(a $ j-1) 5 |]
-               f' = f j (b $ j-1) (c $ j-1) (d $ j-1)
+    name = mkName "round1"
+    cls = clause [tupP $ args1 ++ args2] (normalB (LetE <$> step <*>
+                                                   tupE (out1 ++ out2))) []
+    args1 = map (flip subP (15 :: Int)) ["a","b","c","d","e"]
+    args2 = map (subP "w") [16..31 :: Int]
+    out1 = map (flip subE (31 :: Int)) ["a","b","c","d","e"]
+    out2 = map (subE "w") [32..47 :: Int]
+    step = declarations [wdecs,adecs,cdecs,restdecs] [16..31]
+    typeSig = sigD name $ appT (appT arrowT tup) tup
+    tup = foldl (\x _ -> appT x wordtype) (tupleT 21) [1..21 :: Int]
+    wordtype = (conT ''Word32BE)
 
-    cdecs :: Int -> DecsQ
-    cdecs = variable' "c" ''Word32BE body
-      where
-        body j = [| rotateL $(b $ j-1) 30 |]
+-- | 32 - 47
+round2f :: DecsQ
+round2f = sequence $ [typeSig, funD name [cls]]
+  where
+    name = mkName "round2"
+    cls = clause [tupP $ args1 ++ args2] (normalB (LetE <$> step <*>
+                                                   tupE (out1 ++ out2))) []
+    args1 = map (flip subP (31 :: Int)) ["a","b","c","d","e"]
+    args2 = map (subP "w") [32..47 :: Int]
+    out1 = map (flip subE (47 :: Int)) ["a","b","c","d","e"]
+    out2 = map (subE "w") [48..63 :: Int]
+    step = declarations [wdecs,adecs,cdecs,restdecs] [32..47]
+    typeSig = sigD name $ appT (appT arrowT tup) tup
+    tup = foldl (\x _ -> appT x wordtype) (tupleT 21) [1..21 :: Int]
+    wordtype = (conT ''Word32BE)
 
-    wdecs :: Int -> DecsQ
-    wdecs = variable' "w" ''Word32BE body
-      where
-        body j | j<16      = subE "m" j
-               | otherwise = [| rotateL ($(w $ j-3)  `xor` $(w $ j-8)  `xor`
-                                         $(w $ j-14) `xor` $(w $ j-16)) 1 |]
 
-    kdecs :: Int -> DecsQ
-    kdecs = variable' "k" ''Word32BE body
-      where
-        body j  | j <= 19    = [| 0x5a827999 :: Word32BE |]
-                | j <= 39    = [| 0x6ed9eba1 :: Word32BE |]
-                | j <= 59    = [| 0x8f1bbcdc :: Word32BE |]
-                | otherwise  = [| 0xca62c1d6 :: Word32BE |]
+-- | 48 - 63
+round3f :: DecsQ
+round3f = sequence $ [typeSig, funD name [cls]]
+  where
+    name = mkName "round3"
+    cls = clause [tupP $ args1 ++ args2] (normalB (LetE <$> step <*>
+                                                   tupE (out1 ++ out2))) []
+    args1 = map (flip subP (47 :: Int)) ["a","b","c","d","e"]
+    args2 = map (subP "w") [48..63 :: Int]
+    out1 = map (flip subE (63 :: Int)) ["a","b","c","d","e"]
+    out2 = map (subE "w") [64..79 :: Int]
+    step = declarations [wdecs,adecs,cdecs,restdecs] [48..63]
+    typeSig = sigD name $ appT (appT arrowT tup) tup
+    tup = foldl (\x _ -> appT x wordtype) (tupleT 21) [1..21 :: Int]
+    wordtype = (conT ''Word32BE)
 
-    restdecs :: Int -> DecsQ
-    restdecs = permute [("e","d"),("d","c"),("b","a")]
+-- | 64 - 79
+round4f :: DecsQ
+round4f = sequence $ [typeSig,funD name [cls]]
+  where
+    name = mkName "round4"
+    cls = clause [tupP $ args1 ++ args2] (normalB (LetE <$> step <*>
+                                                   tupE out1)) []
+    args1 = map (flip subP (63 :: Int)) ["a","b","c","d","e"]
+    args2 = map (subP "w") [64..79 :: Int]
+    out1 = map (flip subE (79 :: Int)) ["a","b","c","d","e"]
+    step = declarations [adecs,cdecs,restdecs] [64..79]
+    typeSig = sigD name $ appT (appT arrowT tup) tupout
+    tup = foldl (\x _ -> appT x wordtype) (tupleT 21) [1..21 :: Int]
+    tupout = foldl (\x _ -> appT x wordtype) (tupleT 5) [1..5 :: Int]
+    wordtype = (conT ''Word32BE)
+
+adecs :: Int -> DecsQ
+adecs = variable' "a" ''Word32BE body
+  where
+    body j = [| $(r') + f j $(b $ j-1) $(c $ j-1) $(d $ j-1) + $(e $ j-1) + $(k j) + $(w j) :: Word32BE |]
+      where r' = [| rotateL $(a $ j-1) 5 |]
+
+cdecs :: Int -> DecsQ
+cdecs = variable' "c" ''Word32BE body
+  where
+    body j = [| rotateL $(b $ j-1) 30 |]
+
+wdecs :: Int -> DecsQ
+wdecs i = variable' "w" ''Word32BE body (i+16)
+  where
+    body j = [| rotateL ($(w $ j-3)  `xor` $(w $ j-8)  `xor`
+                         $(w $ j-14) `xor` $(w $ j-16)) 1 |]
+
+k :: Int -> ExpQ
+k j | j <= 19    = [| 0x5a827999 :: Word32BE |]
+    | j <= 39    = [| 0x6ed9eba1 :: Word32BE |]
+    | j <= 59    = [| 0x8f1bbcdc :: Word32BE |]
+    | otherwise  = [| 0xca62c1d6 :: Word32BE |]
+
+restdecs :: Int -> DecsQ
+restdecs = permute [("e","d"),("d","c"),("b","a")]
 
 -- | The round functions
-f :: Int -> ExpQ -> ExpQ -> ExpQ -> ExpQ
-f i x y z | i <= 19   = [| ($(x) .&. $(y)) `xor` (complement $(x) .&. $(z)) |]
-          | i <= 39   = [| $(x) `xor` $(y) `xor` $(z) |]
-          | i <= 59   = [| ($(x) .&. $(y)) `xor` ($(y) .&. $(z)) `xor` ($(z)
-                           .&. $(x)) |]
-          | otherwise = [| $(x) `xor` $(y) `xor` $(z) |]
+f :: Int -> Word32BE -> Word32BE -> Word32BE -> Word32BE
+{-# INLINE f #-}
+f i x y z | i <= 19   = ch x y z
+          | i <= 39   = parity x y z
+          | i <= 59   = maj x y z
+          | otherwise = parity x y z
 
-a,b,c,d,e,k,w :: Int -> ExpQ
+ch :: Word32BE -> Word32BE -> Word32BE -> Word32BE
+{-# INLINE ch #-}
+ch x y z = (x .&. y) `xor` (complement x .&. z)
+
+parity :: Word32BE -> Word32BE -> Word32BE -> Word32BE
+{-# INLINE parity #-}
+parity x y z = x `xor` y `xor` z
+
+-- same as (x .&. y) `xor` (y .&. z) `xor` (z .&. x)
+maj :: Word32BE -> Word32BE -> Word32BE -> Word32BE
+{-# INLINE maj #-}
+maj x y z = (x .&. (y .|. z)) .|. (y .&. z)
+
+
+a,b,c,d,e,w :: Int -> ExpQ
 a = subE "a"
 b = subE "b"
 c = subE "c"
 d = subE "d"
 e = subE "e"
-k = subE "k"
 w = subE "w"
